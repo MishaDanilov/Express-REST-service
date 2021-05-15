e62ce7da3b68d15ad8d20b4e6632bdb1bbfe9744
Misha Danilov <62474815+MishaDanilov@users.noreply.github.com>
2021-05-16 00:21:57 +0300

    feat: add user implementation

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,8 +1,10 @@
 const uuid = require('uuid');
 
 class User {
+  static instances = []
+
   constructor({
-    id = uuid(),
+    id = uuid.v4(),
     name = 'USER',
     login = 'user',
     password = 'P@55w0rd'
@@ -11,8 +13,10 @@ class User {
     this.name = name;
     this.login = login;
     this.password = password;
+    User.instances.push(this)
   }
 
+  
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };

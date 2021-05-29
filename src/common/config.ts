d9305649d028ba0bec9beaf3fc14e24e40880343
Misha Danilov <62474815+MishaDanilov@users.noreply.github.com>
2021-05-29 21:51:39 +0300

    feature: config redefinition

---
+++ src/common/config.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
+
+interface Config {
+  PORT: string | undefined;
+  NODE_ENV: string | undefined;
+  MONGO_CONNECTION_STRING: string | undefined;
+  JWT_SECRET_KEY: string | undefined;
+  AUTH_MODE: boolean;
+}
+
+const config: Config = {
+  PORT: process.env['PORT'],
+  NODE_ENV: process.env['NODE_ENV'],
+  MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
+  JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
+  AUTH_MODE: process.env['AUTH_MODE'] === 'true',
+};
+
+const { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE } = config;
+
+export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE };

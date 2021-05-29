0abd3acdd0867282340a434c85e8cd88245526c4
Misha Danilov <62474815+MishaDanilov@users.noreply.github.com>
2021-05-29 21:50:09 +0300

    feature: board redefinition

---
+++ src/resources/boards/board.service.ts
@@ -0,0 +1,18 @@
+import * as boardsRepo from './board.memory.repository';
+import * as tasksRepo from '../tasks/task.memory.repository';
+import { IBoard, IBoardParams, IBoardResponse } from './board.model';
+
+const getAll = (): Promise<Array<IBoard>> => boardsRepo.getAll();
+
+const CreatBoard = (board: IBoardParams): Promise<IBoard> => boardsRepo.CreatBoard(board);
+
+const getBoardByID = (id: string): Promise<IBoard | undefined> => boardsRepo.getBoardByID(id);
+
+const UpdateBoard = (id: string, board: IBoardResponse): Promise<boolean | IBoardResponse> =>
+  boardsRepo.UpdateBoard(id, board);
+
+const DeleteBoard = (id: string): Promise<boolean | { message: string }> => {
+  tasksRepo.DeleteTask('all', id);
+  return boardsRepo.DeleteBoard(id);
+};
+export { getAll, CreatBoard, getBoardByID, UpdateBoard, DeleteBoard };

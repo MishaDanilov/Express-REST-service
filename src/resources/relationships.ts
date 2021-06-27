6cabdb92bf7b0a15bf974b4ab341a9ac6be15f08
Misha Danilov <62474815+MishaDanilov@users.noreply.github.com>
2021-06-27 21:55:25 +0300

    feat: add admin

--- src/resources/relationships.ts
+++ src/resources/relationships.ts
@@ -3,6 +3,7 @@ import { User } from './users/user.model';
 import { Board } from './boards/board.model';
 import { Column } from './columns/column.model';
 import { Task } from './tasks/task.model';
+import { CreatUser, getUserByLogin } from './users/user.service';
 
 Board.hasMany(Column, { onDelete: 'cascade' });
 
@@ -10,6 +11,16 @@ User.hasMany(Task, { sourceKey: 'id', foreignKey: 'userId' });
 
 Board.hasMany(Task, { onDelete: 'cascade', sourceKey: 'id', foreignKey: 'boardId' });
 
-sequelize.sync().catch((error): void => {
-  console.log('Error:', error.message);
-});
+// sequelize.sync().catch((error): void => {
+//   console.log('Error:', error.message);
+// });
+
+(async function initDB() {
+  try {
+    await sequelize.sync();
+  } catch (error) {
+    console.log(error.message);
+  }
+  const admin = await getUserByLogin('admin');
+  if (!admin) await CreatUser({ name: 'admin', login: 'admin', password: 'admin' });
+})();

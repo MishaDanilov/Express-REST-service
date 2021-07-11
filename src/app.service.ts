4fe68b2dafa9fb9915dc94a6deffad830bf24285
Misha Danilov <62474815+MishaDanilov@users.noreply.github.com>
2021-07-11 22:15:34 +0300

    feat: add app implementation

---
+++ src/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from './users/users.service';
+
+@Injectable()
+export class AppService {
+  constructor(private usersService: UsersService) {}
+  async createAdmin() {
+    const admin = await this.usersService.findBylogin('admin');
+    if (!admin)
+      await this.usersService.create({
+        name: 'admin',
+        login: 'admin',
+        password: 'admin',
+      });
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
